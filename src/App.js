0c28bf7e621a4f4272129d56102a8bc0de612181
yasir alshekahi <yaaserjoker1@gmail.com>
2024-11-25 20:11:35 +0300

    f

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ export default function App() {
           style={{
             display: "flex",
             justifyContent: "center",
+            alignItems:'center',
           }}
         >
           <Box />
